1681636320e631b7b1190a2c973b86df980b019d
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2026-02-22 00:21:36 +0300

    add route_constants

---
+++ bloc_app/lib/core/config/router/route_constants.dart
@@ -0,0 +1,37 @@
+abstract class RoutePaths {
+  static const String splash = '/splash';
+  static const String login = '/login';
+  static const String signup = '/signup';
+
+  //tabs
+  static const String post = '/post';
+  static const String search = '/search';
+  static const String profile = '/profile';
+
+  //sub-routes
+
+  //post-detail/:postId/edit
+  static const String postEdit = 'edit';
+  //profile/edit
+  static const String profileEdit = 'edit';
+
+  static const String postCreate = '/create';
+  static const String postDetail = '/post-detail/:postId';
+  static const String userDetail = '/user/:postId';
+}
+
+abstract class RouteNames {
+  static const String splash = 'splash';
+  static const String login = 'login';
+  static const String signup = 'signup';
+
+  static const String post = 'post';
+  static const String search = 'search';
+  static const String profile = 'profile';
+
+  static const String postDetail = 'postDetail';
+  static const String postEdit = 'postEdit';
+  static const String postCreate = 'postCreate';
+  static const String profileEdit = 'profileEdit';
+  static const String userDetail = 'userDetail';
+}
